35d4d87d43fb086445b5bbbd54e7e4ee5a71b614
Mason Lai <47253466+masonlai@users.noreply.github.com>
2021-09-21 10:06:33 -0400

    init

---
+++ src/main/java/com/masonlai/FlashCards/FlashCardsApplication.java
@@ -0,0 +1,13 @@
+package com.masonlai.FlashCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardsApplication.class, args);
+	}
+
+}
